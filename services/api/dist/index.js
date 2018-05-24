278145287b0b8732663f7e6230e6aaec235109f8
ecrofeg <ecrofeg1@gmail.com>
2018-05-24 22:34:03 +0400

    Express init

--- services/api/dist/index.js
+++ services/api/dist/index.js
@@ -1,2 +1,13 @@
-console.log('Hello world!');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+const routes_1 = require("./routes");
+dotenv_1.config();
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes_1.default);
+app.set('port', process.env.PORT || 5555);
+app.listen(app.get('port'));
 //# sourceMappingURL=index.js.map
--- services/api/src/index.ts
+++ services/api/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import { config as dotenv } from 'dotenv';
+import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as path from 'path';
+import routes from './routes';
+
+dotenv();
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', routes);
+app.set('port', process.env.PORT || 5555);
+app.listen(app.get('port'));
